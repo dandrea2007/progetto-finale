e7a996394093b270f25d8fead3939bd833bfba1d
dandrea2007 <alessandro.dandreag@gmail.com>
2023-05-12 13:05:06 +0200

    changed the number of abete

--- sketch.js
+++ sketch.js
@@ -1,32 +1,54 @@
-/**
-
-  Duplicate this sketch to start your project 🧪
-  More info: https://dev.codemotionkids.com/libraries/diorama/docs/
-  
-  Creative Commons BY-SA
-
-**/
-
-function preload() {
-  treeModel = loadAsset("albero.glb");
-}
-
 function setup() {
   createCanvas3D(windowWidth, windowHeight);
+  environment (SUNSET)
   
-  //Change background color of the scene
-  background3D("#333333");
+  var quercia = beginGroup()
+  diffuse("#3F2300");
+  cylinder(0, 1, 0, 0.5, 2);
+  roughness(1);
+  diffuse("#013509")
+  roughness(0.1);
+  sphere(0, 2.5, 0, 1.5)
+  sphere(1, 2 , 0, 1)
+  endGroup();
+  var lato=30
+  var numeroalberi=30;
+for (var i=0; i<numeroalberi; i++) {
+  var x=random (-lato/2, lato/2);
+  var y=0;
+  var z=random (-lato/2, lato/2);
+    clone(quercia, x, y, z);
+}
   
-  hideGrid();
-  
-  var tree = clone(treeModel,0,0,0);
-
+  var abete = beginGroup()
+  diffuse("#3F2300");
+ cylinder(-3, 0.5, 0, 0.5, 1);
+  diffuse("#002506")
+  roughness(0.1);
+  cone(-3, 2.5, 0, 1, 3.5);
+  clone(abete, -1, 0, 2)
+  endGroup();
+   var lato=31
+  var numeroalberi=10;
+for (var i=0; i<numeroalberi; i++) {
+  var x=random (-lato/2, lato/2);
+  var y=0;
+  var z=random (-lato/2, lato/2);
+    clone(abete, x, y, z);
+  abete.setScale(random(1, 1.5));
+}
+  var terreno= beginGroup()
   align(BOTTOM);
-  diffuse("grey");
-  box(0, 0, 0, 6, 0.5, 6);
+  diffuse("#005C0F");
+  box(0,0,0,lato, 1, lato);
+  endGroup();
+  pushFX(BLOOM,0.5);
+  //pushFX(GLITCH)
+  
+  
+
 }
 
 function draw() {
-  // 🔍 Never create objects here, but don't forget to have it in your code:
-  // leave empty for the moment
+  
 }
